703b5d841c83e9fb712ca8573add5b50ab1c49b2
Domashnie Zadaniya <Elcan4361@gmail.com>
2025-09-24 00:37:44 +0400

    Win Form Password

---
+++ MyForm/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MyForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            InputPassword = new TextBox();
+            CheckPassword = new Button();
+            EnterPassword = new Label();
+            SuspendLayout();
+            // 
+            // InputPassword
+            // 
+            InputPassword.Location = new Point(228, 192);
+            InputPassword.Name = "InputPassword";
+            InputPassword.PasswordChar = '*';
+            InputPassword.Size = new Size(278, 23);
+            InputPassword.TabIndex = 0;
+            InputPassword.TextChanged += textBox1_TextChanged;
+            // 
+            // CheckPassword
+            // 
+            CheckPassword.Location = new Point(325, 238);
+            CheckPassword.Name = "CheckPassword";
+            CheckPassword.Size = new Size(94, 67);
+            CheckPassword.TabIndex = 1;
+            CheckPassword.Text = "Check";
+            CheckPassword.UseVisualStyleBackColor = true;
+            CheckPassword.Click += CheckPassword_Click;
+            // 
+            // EnterPassword
+            // 
+            EnterPassword.AutoSize = true;
+            EnterPassword.Font = new Font("Gadugi", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            EnterPassword.Location = new Point(254, 122);
+            EnterPassword.Name = "EnterPassword";
+            EnterPassword.Size = new Size(239, 38);
+            EnterPassword.TabIndex = 2;
+            EnterPassword.Text = "EnterPassword";
+            EnterPassword.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.BlanchedAlmond;
+            ClientSize = new Size(800, 450);
+            Controls.Add(EnterPassword);
+            Controls.Add(CheckPassword);
+            Controls.Add(InputPassword);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox InputPassword;
+        private Button CheckPassword;
+        private Label EnterPassword;
+    }
+}
---
+++ MyForm/Form1.cs
@@ -0,0 +1,102 @@
+﻿namespace MyForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckPassword_Click(object sender, EventArgs e)
+        {
+            // mene bir hisse maraqlidi, indi c# kecmeyenler bu misali nece yazsin, men indi Akademiyada oxuyuram diye DotNet kecmisem
+            // kodun her bir hissesini izah ede bilerem eger lazim olarsa
+
+            string password = InputPassword.Text;
+            string errorMessage = "";
+
+            if (string.IsNullOrEmpty(password))
+            {
+                errorMessage = "Şifrə boş ola bilməz.";
+            }
+            else if (password.Length < 8)
+            {
+                errorMessage = "Şifrədə ən azı 8 simvol olmalıdır.";
+            }
+            else if (!password.Any(char.IsUpper))
+            {
+                errorMessage = "Şifrədə ən azı bir böyük hərf (A-Z) olmalıdır.";
+            }
+            else if (!password.Any(char.IsLower))
+            {
+                errorMessage = "Şifrədə ən azı bir kiçik hərf (a-z) olmalıdır.";
+            }
+            else if (!password.Any(char.IsDigit))
+            {
+                errorMessage = "Şifrədə ən azı bir rəqəm (0-9) olmalıdır.";
+            }
+            else if (!password.Any(c => "@#$%!*".Contains(c)))
+            {
+                errorMessage = "Şifrədə ən azı bir xüsusi işarə (@, #, $, %, !, *) olmalıdır.";
+            }
+            else
+            {
+
+                bool isPasswordHasSequence = false;
+                for (int i = 0; i < password.Length - 2; i++)
+                {
+
+                    if (char.IsDigit(password[i]) && char.IsDigit(password[i + 1]) && char.IsDigit(password[i + 2]))
+                    {
+                        int first = password[i] - '0';
+                        int second = password[i + 1] - '0';
+                        int third = password[i + 2] - '0';
+
+                        if (second == first + 1 && third == second + 1)
+                        {
+                            isPasswordHasSequence = true;
+                            break;
+                        }
+                    }
+
+                    if (char.IsLetter(password[i]) && char.IsLetter(password[i + 1]) && char.IsLetter(password[i + 2]))
+                    {
+                        int first = char.ToLower(password[i]);
+                        int second = char.ToLower(password[i + 1]);
+                        int third = char.ToLower(password[i + 2]);
+
+                        if (second == first + 1 && third == second + 1)
+                        {
+                            isPasswordHasSequence = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (isPasswordHasSequence)
+                {
+                    errorMessage = "Şifrədə '123' və ya 'abc' kimi ardıcıl simvollar olmamalıdır.";
+                }
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                MessageBox.Show(errorMessage, "Xəta");
+            }
+            else
+            {
+                MessageBox.Show("Şifrə qəbul edildi!", "Uğurlu");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
